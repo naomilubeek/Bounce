bd9a15d580780cb3b198fe982f783f67a45fdf4d
naomilubeek <i.am.naomi04@icloud.com>
2021-09-06 11:22:02 +0000

    bal bounced

--- script.js
+++ script.js
@@ -3,12 +3,19 @@
  * de code in deze functie wordt één keer uitgevoerd door
  * de p5 library, zodra het spel geladen is in de browser
  */
+
+var x = 50;
+var y = 50;
+
+var speedX = 10;
+var speedY = 10;
+
 function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
 
   // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
+  
 }
 
 
@@ -18,9 +25,32 @@ function setup() {
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
 function draw() {
+  background(153, 153, 255);
   // stel vulkleur in
-  fill(100, 100, 255);
+  fill(255, 255, 255);
 
   // teken een cirkel
-  ellipse(50,50,80,80);
+  ellipse(x,y,80,80);
+
+  x = x + speedX;
+  y = y + speedY;
+  
+if (y===680){
+  speedY = speedY * -1
 }
+
+if (x===1240){
+  speedX = speedX * -1
+}
+
+if(y===40){
+  speedY= speedY * -1
+}
+
+if(x===40){
+  speedX = speedX * -1
+}
+
+}
+
+ 
