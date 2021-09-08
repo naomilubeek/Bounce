fc07e399b6d7b95fb95c067a4864ab294577cbb1
naomilubeek <i.am.naomi04@icloud.com>
2021-09-08 16:42:42 +0000

    test2

--- script.js
+++ script.js
@@ -38,25 +38,25 @@ function draw() {
   eenX = eenX + speedEenX; // zorgt dat bal beweegt
   eenY = eenY + speedEenY;
   
-if (y===680){
+if (eenY===680){
   speedEenY = speedEenY * -1
 }
 
-if (x===1240){
+if (eenX===1240){
   speeEenX = speedEenX * -1
 }
 
-if(y===40){
+if(eenY===40){
   speedEenY= speedEenY * -1
 }
 
-if(x===40){
+if(eenX===40){
   speedEenX = speedEenX * -1
 }
 
 background(153, 153, 255);
 // stel vulkleur in
-fill(0, 0, 0);
+fill(255, 255, 255);
 
   // teken een cirkel
   ellipse(tweeX,tweeY,80,80);
@@ -64,19 +64,19 @@ fill(0, 0, 0);
   tweeX = tweeX + speedTweeX; // zorgt dat bal beweegt
   tweeY = tweeY + speedTweeY;
   
-if (y===680){
+if (tweeY===680){
   speedTweeY = speedTweeY * -1
 }
 
-if (x===1240){
+if (tweeX===1240){
   speedTweeX = speedTweeX * -1
 }
 
-if(y===40){
+if(tweeY===40){
   speedTweeY= speedTweeY * -1
 }
 
-if(x===40){
+if(tweeX===40){
   speedTweeX = speedTweeX * -1
 }
 
