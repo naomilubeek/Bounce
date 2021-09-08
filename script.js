05eba31c0ede6e2c14cf5c3f3485e196db30ff63
naomilubeek <i.am.naomi04@icloud.com>
2021-09-08 21:13:28 +0000

    het werkt

--- script.js
+++ script.js
@@ -21,12 +21,12 @@ var speedX1 = 3;
 var speedY1 = 3;
 var speedX2 = 5;
 var speedY2 = 5;
-var speedX3 = 5;
-var speedY3 = 5;
-var speedX4 = 5;
-var speedY4 = 5;
-var speedX5 = 5;
-var speedY5 = 5;
+var speedX3 = 4;
+var speedY3 = 4;
+var speedX4 = 6;
+var speedY4 = 6;
+var speedX5 = 7;
+var speedY5 = 7;
 
 /**
  * setup
@@ -50,7 +50,7 @@ function draw() {
   
   
   // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
+  background('black');
 
   // stel vulkleur in
   fill(255, 255, 255);
@@ -63,23 +63,23 @@ function draw() {
   y1 = y1 + speedY1;
 
   // laat stuiteren tegen de onderkant
-  if (y1 === 680) {
+  if (y1 >= 680) {
     speedY1 = speedY1 * -1;
   }
 
-  if (x1 === 1240){
+  if (x1 >= 1240){
     speedX1 = speedX1 * -1;
   }
 
-  if (y1 === 40) {
+  if (y1 <= 40) {
     speedY1 = speedY1 * -1;
   }
 
-  if (x1 === 40) {
+  if (x1 <= 40) {
     speedX1 = speedX1 * -1;
   }
 
-  fill(0,0,0);
+  fill(255,255,0);
 
    // teken een cirkel
    ellipse(x2, y2, 80, 80);
@@ -89,19 +89,98 @@ function draw() {
    y2 = y2 + speedY2;
  
    // laat stuiteren tegen de onderkant
-   if (y2 === 680) {
+   if (y2 >= 680) {
      speedY2 = speedY2 * -1;
    }
  
-   if (x2 === 1240){
+   if (x2 >= 1240){
      speedX2 = speedX2 * -1;
    }
  
-   if (y2 === 40) {
+   if (y2 <= 40) {
      speedY2 = speedY2 * -1;
    }
  
-   if (x2 === 40) {
+   if (x2 <= 40) {
      speedX2 = speedX2 * -1;
+   }
+
+   fill(255,40,60);
+
+   // teken een cirkel
+   ellipse(x3, y3, 80, 80);
+ 
+   //positie updaten
+   x3 = x3 + speedX3;
+   y3 = y3 + speedY3;
+ 
+   // laat stuiteren tegen de onderkant
+   if (y3 >= 680) {
+     speedY3 = speedY3 * -1;
+   }
+ 
+   if (x3 >= 1240){
+     speedX3 = speedX3 * -1;
+   }
+ 
+   if (y3 <= 40) {
+     speedY3 = speedY3 * -1;
+   }
+ 
+   if (x3 <= 40) {
+     speedX3 = speedX3 * -1;
+   }
+
+   fill(50,60,255);
+
+   // teken een cirkel
+   ellipse(x4, y4, 80, 80);
+ 
+   //positie updaten
+   x4 = x4 + speedX4;
+   y4 = y4 + speedY4;
+ 
+   // laat stuiteren tegen de onderkant
+   
+   if (y4 >= 680) {
+     speedY4 = speedY4 * -1;
+   }
+ 
+   if (x4 >= 1240){
+     speedX4 = speedX4 * -1;
+   }
+ 
+   if (y4 <= 40) {
+     speedY4 = speedY4 * -1;
+   }
+ 
+   if (x4 <= 40) {
+     speedX4 = speedX4 * -1;
+   }
+
+   fill(0,200,0);
+
+   // teken een cirkel
+   ellipse(x5, y5, 80, 80);
+ 
+   //positie updaten
+   x5 = x5 + speedX5;
+   y5 = y5 + speedY5;
+ 
+   // laat stuiteren tegen de onderkant
+   if (y5 >= 680) {
+     speedY5 = speedY5 * -1;
+   }
+ 
+   if (x5 >= 1240){
+     speedX5 = speedX5 * -1;
+   }
+ 
+   if (y5 <= 40) {
+     speedY5 = speedY5 * -1;
+   }
+ 
+   if (x5 <= 40) {
+     speedX5 = speedX5 * -1;
    }
 }
